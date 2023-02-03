86a1edac4f6200bf1427a09c4794f1f169d0d36b
GbCderr14 <114488917+GbCderr14@users.noreply.github.com>
2023-02-04 03:19:23 +0530

    final commit

---
+++ build/precache-manifest.c0a878599a44313335765cf8e12b70d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ba51924daf306eaa1845e204dd0f739",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7693d248128df47057e",
+    "url": "/static/css/main.c736773f.chunk.css"
+  },
+  {
+    "revision": "94de86dd3f4a71078f83",
+    "url": "/static/js/2.79b0603c.chunk.js"
+  },
+  {
+    "revision": "6b4c7bad46f8f7931a3a3a16e9f9f929",
+    "url": "/static/js/2.79b0603c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7693d248128df47057e",
+    "url": "/static/js/main.5fc44872.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.5fc44872.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4054bc057732f69f878d",
+    "url": "/static/js/runtime-main.13ba5424.js"
+  }
+]);
